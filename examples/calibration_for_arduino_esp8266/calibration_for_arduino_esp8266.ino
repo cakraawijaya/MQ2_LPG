12ce91795ff7dfc42ddbab5e27d25fb96ba68ac3
Devan Cakra Mudra Wijaya <devancakra6@gmail.com>
2025-01-14 16:28:42 +0700

    Update: example files

--- examples/calibration_for_arduino_esp8266/calibration_for_arduino_esp8266.ino
+++ examples/calibration_for_arduino_esp8266/calibration_for_arduino_esp8266.ino
@@ -2,14 +2,14 @@
 // By: Devan Cakra M.W
 
 #include "MQ2_LPG.h" // library declaration
-#define MQ2PIN 35 // mq2 pin declaration
+#define MQ2PIN A0 // mq2 pin declaration
 
 MQ2Sensor mq2(MQ2PIN); // create a new object with the name mq2 to hold the MQ2Sensor class
 
 #define RL 100 // 100K ohm
-#define Ro 5.46
+#define Ro 6.02
 #define Volt 5.0
-#define ADC 4095.0 // maximum adc resolution on ESP32 development boards
+#define ADC 1023.0 // maximum adc resolution on Arduino and ESP8266 development boards
 #define x 497.4177875376839
 #define x1 199.150007852152
 #define x2 797.3322752256328
@@ -18,15 +18,12 @@ MQ2Sensor mq2(MQ2PIN); // create a new object with the name mq2 to hold the MQ2S
 #define y2 0.8990240080541785
 
 void setup() {
-  Serial.begin(115200); // default baudrate for the ESP32 development boards
+  Serial.begin(9600); // default baudrate for the Arduino and ESP8266 development boards
   mq2.begin(); // initiate mq2 sensor
-
-  // set calibration
-  mq2.setCalibration(RL, Ro, Volt, ADC, x, x1, x2, y, y1, y2); // comment if you have calibrated
+  mq2.setCalibration(RL, Ro, Volt, ADC, x, x1, x2, y, y1, y2); // set calibration
 }
 
 void loop() {  
-  mq2.readGas(); // reading mq2 sensor data
-  mq2.viewGasData(); // print to serial monitor: gas value & status
+  mq2.viewCalibrationData(); // print to serial monitor: data calibration
   delay(3000); // delay for 3 seconds
 }
